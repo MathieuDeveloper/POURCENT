2074881e1fb45041e3c133e806c603d88c002201
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-08-05 00:47:27 +0200

    some js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,23 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World !");
+// get values
+function getValues() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    
+    let numbers = generateNumbers();
+
+}
+
+
+// generate numbers
+function generateNumbers(sValue, eValue) {
+    let numbers = [];
+    return numbers;
+    for (let index = sValue; index <= eValue; index++) {       
+        numbers.push(index);
+    }
+}
+
+// display numbers
+function displayNumbers() {
+    
 }
