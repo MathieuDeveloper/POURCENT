fc73a38d21dbaf4c394e4bada94817fc3be07b6a
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-08-07 07:41:48 +0200

    building js

--- js/site.js
+++ js/site.js
@@ -1,23 +1,38 @@
 // get values
+alert("hello before");
+
 function getValues() {
+
+    alert("hello");
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
-    
-    let numbers = generateNumbers();
 
+    // get integers
+    let startValue = parseInt(startValue);
+    let endValue = parseInt(endValue);
+
+    let numbers = generateNumbers(startValue, endValue);
+    displayNumbers(numbers);
 }
 
 
 // generate numbers
 function generateNumbers(sValue, eValue) {
     let numbers = [];
-    return numbers;
-    for (let index = sValue; index <= eValue; index++) {       
+
+    for (let index = sValue; index <= eValue; index++) {
         numbers.push(index);
     }
+    return numbers;
 }
 
 // display numbers
-function displayNumbers() {
-    
+function displayNumbers(numbers) {
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index++) {
+
+        let number = numbers[index];
+        templateRows += `<tr><td>${number}</td></tr>`;
+    }
+    document.getElementById("results").innerHTML = templateRows;
 }
